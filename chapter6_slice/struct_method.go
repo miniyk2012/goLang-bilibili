dcddd491067e818daaa8a341faafd459aaed7c25
杨恺 <yangkai02@qutoutiao.net>
2020-02-09 11:23:45 +0800

    overide

--- chapter6_slice/struct_method.go
+++ chapter6_slice/struct_method.go
@@ -17,6 +17,10 @@ func (h *Human) SayHi() {
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
+func (h *Human) SayHello() {
+	fmt.Printf("Hello, I am %s you can call me on %s\n", h.name, h.phone)
+}
+
 func (e *Employee) SayHi() {
 	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
 		e.company, e.phone) //Yes you can split into 2 lines here.
@@ -25,5 +29,7 @@ func (e *Employee) SayHi() {
 func main() {
 	sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
 	sam.SayHi()
-	sam.Human.SayHi() // calls Human.SayHi
+	sam.Human.SayHi()    // calls Human.SayHi
+	sam.SayHello()       // calls Human.SayHello
+	sam.Human.SayHello() // calls Human.SayHello
 }
