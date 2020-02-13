bd1200303e22cb67c759138eb3f518c0c673e408
杨恺 <yangkai02@qutoutiao.net>
2020-02-13 22:44:11 +0800

    20200213

--- aerospike_val/main.go
+++ aerospike_val/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	as "github.com/aerospike/aerospike-client-go"
+	"os"
+	"log"
 )
 
 // This is only for this example.
@@ -22,7 +24,26 @@ func newClient() *as.Client {
 func main() {
 	//localDemo()
 	//runExample(shared.Client)
-
+	people_type := os.Args[1]
+	tuid := os.Args[2]
+	log.Printf("type=%s, tuid=%s", people_type, tuid)
+	users := []string{tuid}
+	switch people_type {
+	case "cvr":
+		scanCvr(users)
+	case "user":
+		scanUsers(users)
+	case "click":
+		scanClick(users)
+	case "show":
+		scanShow(users)
+	case "version":
+		scanVersions(users)
+	case "emptyTag":
+		emptyTag(tuid)
+	default:
+		log.Printf("%s not support", people_type)
+	}
 	//scanAllUser()
 	//log.Println("---------------------------------------------------------------------------")
 	//scanAllVersion()
@@ -32,8 +53,4 @@ func main() {
 	//scanAllBehaviors("show")
 	//log.Println("---------------------------------------------------------------------------")
 	//scanAllBehaviors("cvr")
-	//scanUsers([]string{"--ktYSAvQWekDQUFGC21Sg"})
-	//scanClick([]string{"--DWyUnie5wxFwJdeMIuag"})
-	scanCvr([]string{"-02r0bcUQ92QCYU7dM_S1A"})
-	//scanShow([]string{"-ggHtd993qKScx93HieoRQ"})
 }
--- aerospike_val/prod_validation.go
+++ aerospike_val/prod_validation.go
@@ -6,11 +6,12 @@ import (
 	as "github.com/aerospike/aerospike-client-go"
 	"log"
 )
-const Date string = "2020-02-11"
+const Date string = "2020-02-12"
+const NameSpace = "devicePool2"
 
 func scanAllUser() {
 	client := newClient()
-	recordset, err := client.ScanAll(nil, "devicePool2", "user")
+	recordset, err := client.ScanAll(nil, NameSpace, "user")
 	panicOnError(err)
 	log.Printf("%s:\n", "user")
 	var count = 0
@@ -31,7 +32,7 @@ func scanAllUser() {
 
 func scanAllVersion() {
 	client := newClient()
-	recordset, err := client.ScanAll(nil, "devicePool2", "version")
+	recordset, err := client.ScanAll(nil, NameSpace, "version")
 	panicOnError(err)
 	log.Printf("%s:\n", "version")
 	var count = 0
@@ -52,7 +53,7 @@ func scanAllVersion() {
 
 func scanAllBehaviors(behavior string) {
 	client := newClient()
-	recordset, err := client.ScanAll(nil, "devicePool2", behavior)
+	recordset, err := client.ScanAll(nil, NameSpace, behavior)
 	panicOnError(err)
 	var count = 0
 	log.Printf("%s:\n", behavior)
@@ -74,22 +75,31 @@ func scanUsers(users []string) {
 	client := newClient()
 	readPolicy := as.NewPolicy()
 	for _, tuid := range users {
-		key1, err1 := as.NewKey("devicePool2", "user", tuid)
+		log.Printf("user key=%s", tuid)
+		key1, err1 := as.NewKey(NameSpace, "user", tuid)
 		panicOnError(err1)
-		key2, err2 := as.NewKey("devicePool2", "version", tuid)
-		panicOnError(err2)
-
 		rec1, err3 := client.Get(readPolicy, key1)
 		panicOnError(err3)
 		log.Printf("%s:, %v\n", tuid, rec1.Bins)
 		//log.Printf("%s:, %v\n", tuid, rec1.Bins["tags"].([]interface{}))
-		log.Println()
-		rec2, err4 := client.Get(readPolicy, key2)
-		panicOnError(err4)
-		log.Printf("%s:, %v\n", tuid, rec2.Bins)
 		log.Println("--------------------")
 	}
 }
+
+func scanVersions(users []string) {
+	client := newClient()
+	readPolicy := as.NewPolicy()
+	for _, tuid := range users {
+		log.Printf("version key=%s", tuid)
+		key, err1 := as.NewKey(NameSpace, "version", tuid)
+		panicOnError(err1)
+		rec, err2 := client.Get(readPolicy, key)
+		panicOnError(err2)
+		log.Printf("%s:, %v\n", tuid, rec.Bins)
+		log.Println("--------------------")
+	}
+}
+
 
 
 
@@ -98,7 +108,7 @@ func scanClick(users []string) {
 	readPolicy := as.NewPolicy()
 	for _, tuid := range users {
 		log.Printf("click key=%s", tuid+Date)
-		key, err := as.NewKey("devicePool2", "click", tuid+Date)
+		key, err := as.NewKey(NameSpace, "click", tuid+Date)
 		panicOnError(err)
 
 		rec, err := client.Get(readPolicy, key)
@@ -116,7 +126,7 @@ func scanShow(users []string) {
 	readPolicy := as.NewPolicy()
 	for _, tuid := range users {
 		log.Printf("show key=%s", tuid+Date)
-		key, err := as.NewKey("devicePool2", "show", tuid+Date)
+		key, err := as.NewKey(NameSpace, "show", tuid+Date)
 		panicOnError(err)
 
 		rec, err := client.Get(readPolicy, key)
@@ -133,7 +143,7 @@ func scanCvr(users []string) {
 	readPolicy := as.NewPolicy()
 	for _, tuid := range users {
 		log.Printf("cvr key=%s", tuid+Date)
-		key, err := as.NewKey("devicePool2", "cvr", tuid+Date)
+		key, err := as.NewKey(NameSpace, "cvr", tuid+Date)
 		panicOnError(err)
 
 		rec, err := client.Get(readPolicy, key)
@@ -143,4 +153,28 @@ func scanCvr(users []string) {
 		log.Printf("%s:, %v\n", tuid, rec.Bins)
 		log.Println("--------------------")
 	}
+}
+
+func emptyTag(tuid string) {
+	log.Printf("empty tuid=%s", tuid)
+	client := newClient()
+	readPolicy := as.NewPolicy()
+	writePolicy := as.NewWritePolicy(0, 0)
+
+	key, err := as.NewKey(NameSpace, "user", tuid)
+	panicOnError(err)
+	record, err := client.Get(readPolicy, key, "tags")
+	panicOnError(err)
+	receivedList := record.Bins["tags"].([]interface{})
+	log.Printf("before empty tags=%v\n",receivedList)
+
+	// empty tags
+	empty_tags := []int{}
+	bin := as.NewBin("tags", empty_tags)
+	client.PutBins(writePolicy ,key, bin)
+
+	record, err = client.Get(readPolicy, key, "tags")
+	panicOnError(err)
+	receivedList = record.Bins["tags"].([]interface{})
+	log.Printf("after empty tags=%v\n",receivedList)
 }
