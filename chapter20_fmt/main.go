97cdacb6538a5ada3f35b29d38107758fa279976
杨恺 <yangkai02@qutoutiao.net>
2020-02-24 22:26:34 +0800

    fmt

---
+++ chapter20_fmt/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func demo1() {
+	fmt.Print("在终端打印该信息。")
+	name := "沙河小王子"
+	fmt.Printf("我是：%s\n", name)
+	fmt.Println("在终端打印单独一行显示")
+}
+
+func writeToFile() {
+	fmt.Fprintln(os.Stdout, "向标准输出写入内容")
+	fileObj, err := os.OpenFile("./xx.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("打开文件出错，err:", err)
+		return
+	}
+	name := "沙河小王子"
+	// 向打开的文件句柄中写入内容
+	fmt.Fprintf(fileObj, "往文件中写如信息：%s", name)
+}
+
+func returnString() {
+	s1 := fmt.Sprint("沙河小王子")
+	name := "沙河小王子"
+	age := 18
+	s2 := fmt.Sprintf("name:%s,age:%d", name, age)
+	s3 := fmt.Sprintln("沙河小王子")
+	fmt.Print(s1, s2, s3)
+}
+
+// 自定义错误类型
+func errorF() {
+	err := fmt.Errorf("这是一个%s", "错误")
+	fmt.Println(err)
+}
+
+func demo2() {
+	fmt.Printf("%v\n", 100)
+	fmt.Printf("%v\n", false)
+	fmt.Printf("%t\n", false)
+	o := struct{ name string }{"小王子"}
+	fmt.Printf("%v\n", o)
+	fmt.Printf("%+v\n", o)
+	fmt.Printf("%#v\n", o)
+	fmt.Printf("%T\n", o)
+	fmt.Printf("100%%\n")
+}
+
+func intDemo() {
+	n := 65
+	fmt.Printf("%b\n", n)
+	fmt.Printf("%c\n", n)
+	fmt.Printf("%d\n", n)
+	fmt.Printf("%o\n", n)
+	fmt.Printf("%x\n", n)
+	fmt.Printf("%X\n", n)
+	fmt.Printf("%U\n", n)
+}
+
+func floatDemo() {
+	f := 12.34
+	fmt.Printf("%b\n", f)
+	fmt.Printf("%e\n", f)
+	fmt.Printf("%E\n", f)
+	fmt.Printf("%f\n", f)
+	fmt.Printf("%g\n", f)
+	fmt.Printf("%G\n", f)
+}
+
+func stringDemo() {
+	s := "小王子"
+	fmt.Printf("%s\n", s)
+	fmt.Printf("%q\n", s)
+	fmt.Printf("%x\n", s)
+	fmt.Printf("%X\n", s)
+}
+
+func pointerDemo() {
+	a := 10
+	fmt.Printf("%p\n", &a)
+	fmt.Printf("%#p\n", &a)
+}
+
+func main() {
+	demo1()
+	println()
+	writeToFile()
+	println()
+	returnString()
+	println()
+	errorF()
+	println()
+	demo2()
+	println()
+	intDemo()
+	println()
+	floatDemo()
+	println()
+	stringDemo()
+	println()
+	pointerDemo()
+}
